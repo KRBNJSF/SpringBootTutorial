2fd8d8ebb80b486e00e34714d3792c8012852cc0
erik.reindl <rubes999@seznam.cz>
2023-01-13 12:15:11 +0100

    - update

---
+++ src/main/java/cz/spsmb/controller/ItemController.java
@@ -0,0 +1,35 @@
+package cz.spsmb.controller;
+
+import cz.spsmb.service.SimpleItemService;
+import cz.spsmb.entity.Item;
+import cz.spsmb.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//Toto je controller, ktery ma neco zpracovat behem requestu
+@RestController
+public class ItemController {
+
+    private ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = new SimpleItemService();
+    }
+
+    @RequestMapping(path = "/items", method = RequestMethod.GET)
+    public List<Item> getAllItems() {
+        return itemService.getAll();
+    }
+
+    @RequestMapping(path = "/item", method = RequestMethod.POST)
+    public void saveItem(@RequestBody Item item) {
+        itemService.save(item);
+    }
+
+}
---
+++ src/main/java/cz/spsmb/service/SimpleItemService.java
@@ -0,0 +1,25 @@
+package cz.spsmb.service;
+
+import cz.spsmb.entity.Item;
+import cz.spsmb.service.ItemService;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class SimpleItemService implements ItemService {
+
+    private List<Item> items = new LinkedList<>();
+
+    @Override
+    public List<Item> getAll() {
+        return items;
+    }
+
+    @Override
+    public void save(Item item) {
+        this.items.add(item);
+    }
+
+}
